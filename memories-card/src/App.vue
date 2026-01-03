ec21c965584191401c1df1fb90408596f888e24c
Harun <Aliyevhrn@github.com>
2026-01-03 03:19:38 +0300

    2-button: created a vue component Button

--- memories-card/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const curDate = new Date();
-</script>
-
-<template>
-  <div>{{ curDate.toLocaleString() }}</div>
-</template>
-
-<style scoped></style>
---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <button class="button"><slot /></button>
+</template>
+
+<style scoped>
+.button {
+  font-size: 24px;
+  font-weight: 400;
+  padding: 16px 10px;
+  border-radius: 30px;
+  width: 315px;
+}
+</style>
