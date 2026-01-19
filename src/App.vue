90f53c724221947f1cd20e86bb8bbbbd6d48ad2f
Harun <Aliyevhrn@github.com>
2026-01-19 11:24:48 +0300

    5-templates: add statuses for card

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,107 @@
 <script setup>
+import { ref } from "vue";
 import Button from "./components/Button.vue";
 import Card from "./components/Card.vue";
 import Header from "./components/Header.vue";
+
+const gameStatus = ref(false);
+let scoreNumber = ref(0);
+const wordsList = ref([
+  {
+    id: 1,
+    word: "table",
+    tranlation: "Стол",
+    status: "closed",
+  },
+  {
+    id: 2,
+    word: "pen",
+    tranlation: "Ручка",
+    status: "closed",
+  },
+  {
+    id: 3,
+    word: "task",
+    tranlation: "Задача",
+    status: "closed",
+  },
+  {
+    id: 4,
+    word: "book",
+    tranlation: "Книга",
+    status: "closed",
+  },
+]);
+
+function openCard(card) {
+  card.status = "pending";
+}
+function updateStatus({ id, status }) {
+  const cardIndex = wordsList.value.findIndex((card) => card.id === id);
+  wordsList.value[cardIndex].status = status;
+
+  // Обновляем счет
+  if (status === "success") {
+    scoreNumber.value += 1;
+  }
+  // else if (status === "fail") {
+  //   scoreNumber.value = Math.max(0, scoreNumber.value - 1);
+  // }
+}
+function resetGame() {
+  wordsList.value.forEach((card) => {
+    card.status = "closed";
+  });
+  scoreNumber.value = 0;
+  gameStatus.value = true;
+}
+function startGame() {
+  gameStatus.value = true;
+}
 </script>
 
 <template>
-  <Header />
-  <div class="card-list">
-    <Card />
+  <Header :score-number="scoreNumber" />
+  <div class="card-list" v-if="gameStatus">
+    <Card
+      v-for="card in wordsList"
+      :key="card.id"
+      :id="card.id"
+      :word="card.word"
+      :translation="card.tranlation"
+      :status="card.status"
+      @click.capture="openCard(card)"
+      @fail-card="updateStatus"
+      @success-card="updateStatus"
+    />
   </div>
-  <Button>Начать игру</Button>
+  <div class="btns-list">
+    <Button class="btn-start" @click="startGame()" v-if="!gameStatus"
+      >Начать игру</Button
+    >
+    <Button class="btn-reset" @click="resetGame()" v-if="gameStatus"
+      >Начать заново</Button
+    >
+  </div>
 </template>
+
+<style scoped>
+.card-list {
+  display: flex;
+  justify-content: space-between;
+  gap: 100px;
+}
+.btns-list {
+  display: flex;
+  justify-content: center;
+}
+.btn-start {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.btn-reset {
+  margin-top: 50px;
+}
+</style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,72 @@
 <script setup>
+import Fail from "@/icons/Fail.vue";
+import Success from "@/icons/success.vue";
 import { ref } from "vue";
 
-// const emit = defineEmits(["cardReverse", "statusChange"]);
-// function reverseCard() {
-//   emit("cardReverse");
-// }
-// function changeStatus() {
-//   emit("statusChange");
-// }
+const emit = defineEmits(["fail-card", "success-card"]);
+function handleSuccess() {
+  emit("success-card", {
+    id: props.id,
+    status: "success",
+  });
+}
+function handleFail() {
+  emit("fail-card", {
+    id: props.id,
+    status: "fail",
+  });
+}
 
-const cardData = ref({
-  word: "unadmitted",
-  translation: "не допущенный",
-  state: "closed",
-  status: null,
+const props = defineProps({
+  id: Number,
+  word: String,
+  translation: String,
+  status: "closed" | "pending" | "success" | "fail",
 });
 </script>
 
 <template>
-  <div class="card">
+  <div
+    class="card"
+    :class="{
+      closed: status === 'closed',
+      pending: status === 'pending',
+      success: status === 'success',
+      fail: status === 'fail',
+    }"
+  >
     <div class="card-wrapper">
-      <span class="card-number">01</span>
-      <span class="card-en-text">{{ cardData.word }}</span>
-      <span class="card-ru-text">{{ cardData.translation }}</span>
-      <span class="card-reverse-text">Перевернуть</span>
+      <span class="card-id">{{ id < 10 ? "0" : "" }}{{ id }}</span>
+      <span class="card-correct-icon" v-show="status === 'success'">
+        <Success />
+      </span>
+      <span class="card-fail-icon" v-show="status === 'fail'">
+        <Fail />
+      </span>
+      <span v-show="status === 'closed'" class="card-word">
+        {{ word }}
+      </span>
+      <span
+        v-show="
+          status === 'pending' || status === 'success' || status === 'fail'
+        "
+        class="card-translation"
+        >{{ translation }}</span
+      >
+      <span class="card-status" v-show="status === 'closed'">Перевернуть</span>
+      <span
+        class="card-status"
+        v-show="status === 'success' || status === 'fail'"
+        >Завершено</span
+      >
+      <div class="card-status" v-show="status === 'pending'">
+        <button @click="handleFail">
+          <Fail />
+        </button>
+        <button @click="handleSuccess">
+          <Success />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +86,7 @@ const cardData = ref({
   border-radius: 16px;
   border: 1px solid var(--color-lightblue);
 }
-.card-number {
+.card-id {
   position: absolute;
   left: 16px;
   font-size: 14px;
@@ -52,8 +95,20 @@ const cardData = ref({
   padding: 1px;
   background: var(--color-white);
 }
-.card-en-text,
-.card-ru-text {
+.card-correct-icon,
+.card-fail-icon {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.card-correct-icon {
+  top: -15px;
+}
+.card-fail-icon {
+  top: -20px;
+}
+.card-word,
+.card-translation {
   font-size: 18px;
   font-weight: 400;
   position: absolute;
@@ -61,11 +116,11 @@ const cardData = ref({
   left: 50%;
   transform: translate(-50%, -50%);
 }
-.card-reverse-text {
+.card-status {
   font-size: 12px;
-  font-weight: 700;
+  font-weight: 600;
   line-height: 18px;
-  letter-spacing: 12%;
+  letter-spacing: 0.1rem;
   position: absolute;
   bottom: -10px;
   left: 50%;
@@ -73,5 +128,21 @@ const cardData = ref({
   background: var(--color-white);
   text-transform: uppercase;
   transform: translateX(-50%);
+}
+.card.pending .card-status {
+  display: flex;
+  justify-content: space-between;
+  width: 100px;
+  bottom: -15px;
+  padding: 0 10px;
+}
+.card-status button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+.card-status button svg {
+  width: 24px;
+  height: 24px;
 }
 </style>
