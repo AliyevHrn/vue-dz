726b917b997cf6e03ed73b50c7cc6e26a3344d03
Harun <Aliyevhrn@github.com>
2026-02-08 01:37:42 +0300

    6-lifecycle: add real api

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,69 +1,69 @@
 <script setup>
 import Fail from "@/icons/Fail.vue";
-import Success from "@/icons/success.vue";
+import Success from "@/icons/Success.vue";
 import { ref } from "vue";
 
-const emit = defineEmits(["fail-card", "success-card"]);
-function handleSuccess() {
-  emit("success-card", {
-    id: props.id,
-    status: "success",
-  });
-}
-function handleFail() {
-  emit("fail-card", {
-    id: props.id,
-    status: "fail",
-  });
-}
-
+const cardStatus = ref("closed");
 const props = defineProps({
   id: Number,
   word: String,
   translation: String,
-  status: "closed" | "pending" | "success" | "fail",
 });
+const emit = defineEmits(["successStatus", "failStatus"]);
+function successStatus() {
+  cardStatus.value = "success";
+  emit("successStatus", cardStatus.value);
+}
+function failStatus() {
+  cardStatus.value = "fail";
+  emit("failStatus", cardStatus.value);
+}
 </script>
 
 <template>
   <div
     class="card"
     :class="{
-      closed: status === 'closed',
-      pending: status === 'pending',
-      success: status === 'success',
-      fail: status === 'fail',
+      closed: cardStatus === 'closed',
+      pending: cardStatus === 'pending',
+      success: cardStatus === 'success',
+      fail: cardStatus === 'fail',
     }"
+    @click.capture="cardStatus = 'pending'"
   >
     <div class="card-wrapper">
       <span class="card-id">{{ id < 10 ? "0" : "" }}{{ id }}</span>
-      <span class="card-correct-icon" v-show="status === 'success'">
+      <span class="card-correct-icon" v-show="cardStatus === 'success'">
         <Success />
       </span>
-      <span class="card-fail-icon" v-show="status === 'fail'">
+      <span class="card-fail-icon" v-show="cardStatus === 'fail'">
         <Fail />
       </span>
-      <span v-show="status === 'closed'" class="card-word">
+      <span v-show="cardStatus === 'closed'" class="card-word">
         {{ word }}
       </span>
       <span
         v-show="
-          status === 'pending' || status === 'success' || status === 'fail'
+          cardStatus === 'pending' ||
+          cardStatus === 'success' ||
+          cardStatus === 'fail'
         "
         class="card-translation"
         >{{ translation }}</span
       >
-      <span class="card-status" v-show="status === 'closed'">Перевернуть</span>
+      <span class="card-status" v-show="cardStatus === 'closed'"
+        >Перевернуть</span
+      >
       <span
         class="card-status"
-        v-show="status === 'success' || status === 'fail'"
+        v-show="cardStatus === 'success' || cardStatus === 'fail'"
         >Завершено</span
       >
-      <div class="card-status" v-show="status === 'pending'">
-        <button @click="handleFail">
+      <div class="card-status" v-show="cardStatus === 'pending'">
+        <button @click="failStatus">
           <Fail />
         </button>
-        <button @click="handleSuccess">
+        <button @click="successStatus">
           <Success />
         </button>
       </div>
@@ -73,11 +73,11 @@ const props = defineProps({
 
 <style scoped>
 .card {
-  width: 275px;
+  width: calc(25% - 100px);
   height: 376px;
   border-radius: 16px;
   background-color: var(--color-white);
-  padding: 28px 19px;
+  padding: 28px 20px;
   cursor: pointer;
 }
 .card-wrapper {
@@ -111,6 +111,7 @@ const props = defineProps({
 .card-translation {
   font-size: 18px;
   font-weight: 400;
+  text-align: center;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -144,5 +145,25 @@ const props = defineProps({
 .card-status button svg {
   width: 24px;
   height: 24px;
+}
+@media (max-width: 1440px) {
+  .card {
+    width: calc(25% - 50px);
+  }
+}
+@media (max-width: 1200px) {
+  .card {
+    width: calc(25% - 30px);
+  }
+}
+@media (max-width: 1024px) {
+  .card {
+    width: calc(33% - 20px);
+  }
+}
+@media (max-width: 840px) {
+  .card {
+    width: calc(33% - 10px);
+  }
 }
 </style>
